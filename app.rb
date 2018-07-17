b90f9ee8f11e3510560ea9961f03c579c066ffb2
celineboudaie <cboudaie@gmail.com>
2018-07-17 21:54:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,16 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-get '/hours' do
-" "
-end
-gets '/dates' do
-" "
-end
-gets '/menu' do
-" "
-end
-gets '/reserve-pay' do
-""
+get '/' do
+  
+
 end
 end
